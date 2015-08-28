d42c59f809d41a04fa6a7d7d530d5ad7df1df2c1
zhngmeipeng <896153749@qq.com>
2015-08-28 13:38:03 +0800

    aaaaaaaaaaaaa

--- resources/public/js/rightTable.js
+++ resources/public/js/rightTable.js
@@ -22,5 +22,48 @@ window.onload=function(){
             var ss=table+tt+"</table>"
             $("#divmessage").html(ss);
         });
-}
+
+    var servertable=' <table cellspacing="0" >';
+    var st = "";
+
+    $.getJSON(
+        "../json/cc.json",
+        function(data) {
+            $.each(data, function(k, v) {
+                st+='<tr><td rowspan="2" class="first-td"><input type="checkbox"/></td><td class="second-td">服务器名称</td>' +
+                    '<td class="third-td">'+ v.sername+'</td><td class="forth-td">代码</td><td class="five-td">'+ v.code+'</td>' +
+                    '<td class="forth-td">IP</td><td class="five-td">'+ v.ip+'</td><td class="forth-td">位置</td>' +
+                    '<td class="six-td">'+ v.position+'</td><td class="forth-td">网络</td><td class="five-td">v.internate</td></tr>' +
+                    '<tr><td class="second-td">服务器状态</td><td colspan="9" class="last-td">'+v.status+'</td></tr>';
+
+            });
+            var sert=servertable+st+'</table>'
+            $("#serverTable").html(sert);
+        });
+
+    var statable='<table cellspacing="0" >';
+    var statt = '';
+
+    $.getJSON(
+        "../json/dd.json",
+        function(data) {
+            $.each(data, function(k, v) {
+                statt+='<tr><td rowspan="4" class="first-machine-td"><input type="checkbox"/></td><td class="machine-first">台站名称</td>' +
+                    '<td class="machine-second">'+ v.staname+'</td><td class="machine-first">台站代码</td><td class="machine-second">'+ v.stacode+'</td>' +
+                    '<td class="machine-first">测点编码</td><td class="machine-second">'+ v.testcod+'</td></tr>' +
+                    '<tr><td class="machine-first">仪器名称</td>' +
+                    '<td class="machine-second">'+ v.mname+'</td> <td class="machine-first">仪器编号</td><td class="machine-second">'+ v.mcode+'</td>' +
+                    '<td class="machine-first">所属学科</td><td class="machine-second">'+ v.ssub+'</td></tr>' +
+                    '<tr><td class="machine-first">所在位置</td><td class="machine-second">v.pos</td><td class="machine-first">管理员</td>' +
+                    '<td class="machine-second">'+ v.admin+'</td><td class="machine-first">联系电话</td><td class="machine-second">v.tel</td></tr>' +
+                    '<tr><td class="machine-first">仪器状态</td><td colspan="5" class="border-th">'+ v.mstatus+'</td></tr>' +
+                    '<tr><tr class="machine-last-tr"><td colspan="7" class="machine-last-td"></td> </tr>'
+
+            });
+            var status=statable+statt+'</table>';
+            console.log(status)
+            $("#machineTable").html(status);
+        });
+};
+
 }
